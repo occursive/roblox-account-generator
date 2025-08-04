5fbb6a65c51d0c80643bf04eb3c2a248b80b4cf4
Occursive <lFayOKWbDujbksx0EQrtqqx6TNRzaUOQDVvOsdES@proton.me>
2025-08-04 15:30:47 +0200

    Update headers.py

--- headers.py
+++ headers.py
@@ -1,6 +1,25 @@
-def header1(csrf_token):
+def headers1():
+    return {
+    "host": "www.roblox.com",
+    "connection": "keep-alive",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "sec-fetch-site": "none",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-user": "?1",
+    "sec-fetch-dest": "document",
+    "sec-ch-ua": "\"Not)A;Brand\";v=\"8\", \"Chromium\";v=\"138\", \"Google Chrome\";v=\"138\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "en-US,en;q=0.9"
+}
+
+def headers2(csrf_token):
     return {
     "host": "auth.roblox.com",
+    "connection": "keep-alive",
     "sec-ch-ua-platform": "\"Windows\"",
     "x-csrf-token": csrf_token,
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
@@ -16,29 +35,29 @@ def header1(csrf_token):
     "accept-encoding": "gzip, deflate, br, zstd",
     "accept-language": "en-US,en;q=0.9"
 }
-    
-def header2(csrf_token):
-    return {
-    "host": "auth.roblox.com",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "x-csrf-token": csrf_token,
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
-    "accept": "application/json, text/plain, */*",
-    "sec-ch-ua": "\"Not)A;Brand\";v=\"8\", \"Chromium\";v=\"138\", \"Google Chrome\";v=\"138\"",
-    "content-type": "application/json;charset=UTF-8",
-    "sec-ch-ua-mobile": "?0",
-    "origin": "https://www.roblox.com",
-    "sec-fetch-site": "same-site",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-dest": "empty",
-    "referer": "https://www.roblox.com/",
-    "accept-encoding": "gzip, deflate, br, zstd",
-    "accept-language": "en-US,en;q=0.9",
-}
-    
-def header3(csrf_token):
+
+def headers3():
     return {
     "host": "apis.roblox.com",
+    "connection": "keep-alive",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
+    "accept": "application/json, text/plain, */*",
+    "sec-ch-ua": "\"Not)A;Brand\";v=\"8\", \"Chromium\";v=\"138\", \"Google Chrome\";v=\"138\"",
+    "sec-ch-ua-mobile": "?0",
+    "origin": "https://www.roblox.com",
+    "sec-fetch-site": "same-site",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-dest": "empty",
+    "referer": "https://www.roblox.com/",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "en-US,en;q=0.9"
+}
+
+def headers4(csrf_token, host):
+    return {
+    "host": host,
+    "connection": "keep-alive",
     "sec-ch-ua-platform": "\"Windows\"",
     "x-csrf-token": csrf_token,
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
@@ -52,13 +71,14 @@ def header3(csrf_token):
     "sec-fetch-dest": "empty",
     "referer": "https://www.roblox.com/",
     "accept-encoding": "gzip, deflate, br, zstd",
-    "accept-language": "en-US,en;q=0.9",
+    "accept-language": "en-US,en;q=0.9"
 }
-     
-def header4(encrypted_challenge_metadata, csrf_token, challenge_id):
+
+def headers5(encrypted_metadata, csrf_token, challenge_id):
     return {
     "host": "auth.roblox.com",
-    "rblx-challenge-metadata": encrypted_challenge_metadata,
+    "connection": "keep-alive",
+    "rblx-challenge-metadata": encrypted_metadata,
     "sec-ch-ua-platform": "\"Windows\"",
     "x-csrf-token": csrf_token,
     "sec-ch-ua": "\"Not)A;Brand\";v=\"8\", \"Chromium\";v=\"138\", \"Google Chrome\";v=\"138\"",
@@ -75,5 +95,5 @@ def header4(encrypted_challenge_metadata, csrf_token, challenge_id):
     "sec-fetch-dest": "empty",
     "referer": "https://www.roblox.com/",
     "accept-encoding": "gzip, deflate, br, zstd",
-    "accept-language": "en-US,en;q=0.9",
+    "accept-language": "en-US,en;q=0.9"
 }
