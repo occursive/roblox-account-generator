d7e3fe77f50942327277b34b3714abc6bce41922
Occursive <lFayOKWbDujbksx0EQrtqqx6TNRzaUOQDVvOsdES@proton.me>
2025-08-04 15:29:28 +0200

    Update secure_auth.py

--- secure_auth.py
+++ secure_auth.py
@@ -3,9 +3,9 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
 from base64 import b64encode
 from time import time
-import requests
+from headers import headers3
 
-def get_sa(proxy):
+def get_sa(session):
     try:
         private_key = ec.generate_private_key(ec.SECP256R1(), default_backend())
         public_key = private_key.public_key()
@@ -15,8 +15,7 @@ def get_sa(proxy):
         )
         client_public_key = b64encode(spki_bytes).decode('utf-8')
         client_epoch_timestamp = str(int(time()))
-        proxies = {"http": proxy, "https": proxy}
-        response = requests.get(url="https://apis.roblox.com/hba-service/v1/getServerNonce", proxies=proxies)
+        response = session.get(url="https://apis.roblox.com/hba-service/v1/getServerNonce", headers=headers3())
         response.raise_for_status()
         server_nonce = response.text.strip('"')
 
@@ -26,4 +25,4 @@ def get_sa(proxy):
 
         return client_public_key, client_epoch_timestamp, server_nonce, sai_signature
     except Exception:
-        return None, None, None, None
+            return None, None, None, None
